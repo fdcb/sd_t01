63cd00ae31c9c65ce8c4fbf24494c270fb3ddee1
Filipa Brito <Filipa Brito>
2016-04-10 16:40:56 +0100

    Server/rmi working, but gui is dead

--- Client/src/sample/Main.java
+++ Client/src/sample/Main.java
@@ -1,4 +1,5 @@
 package sample;
+import example.RMIInterface;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -6,6 +7,9 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.rmi.Naming;
+import java.rmi.RMISecurityManager;
+import java.util.Date;
 
 public class Main extends Application {
 
@@ -13,8 +17,8 @@ public class Main extends Application {
     public void start(Stage primaryStage){
 		//gotoNewScene(primaryStage, LoginController.FILE_NAME,
 			//	LoginController.WINDOW_TITLE);
-	    gotoNewScene(primaryStage, CheckSolutionsController.FILE_NAME,
-                CheckSolutionsController.WINDOW_TITLE);
+	    gotoNewScene(primaryStage, LoginController.FILE_NAME,
+                LoginController.WINDOW_TITLE);
     }
 
 	/**
@@ -28,7 +32,7 @@ public class Main extends Application {
 	public static void gotoNewScene(Stage stage, String filename, String
 			windowTitle){
 		try {
-			Parent root = FXMLLoader.load(Main.class.getResource
+			Parent root = FXMLLoader.load(sample.Main.class.getResource
 					(filename));
 			stage.setScene(new Scene(root));
 			stage.setTitle(windowTitle);
@@ -39,7 +43,36 @@ public class Main extends Application {
 	}
 
     public static void main(String[] args) {
-        launch(args);
+	    String serverName = "";
+	    System.setProperty("java.security.policy", "C:\\T01SD\\Client\\src" +
+			    "\\java.policy");
+	    System.setSecurityManager(new RMISecurityManager());
+	    if (args.length != 1) {
+		    try {
+			    serverName = java.net.InetAddress.getLocalHost().getHostName();
+		    } catch(Exception e) {
+			    e.printStackTrace();
+		    }
+	    }
+	    else {
+		    serverName = args[0];
+	    }
+	    if (serverName.equals( "") ) {
+		    System.out.println("usage: java Client < host running RMI server>");
+		    System.exit(0);
+	    }
+	    try {
+		    //bind server object to object in client
+		    RMIInterface myServerObject =
+				    (RMIInterface) Naming.lookup("//"+serverName+"/Impl");
+		    //invoke method on server object
+		    Date d = myServerObject.getDate();
+		    System.out.println("Date on server is " + d);
+            launch(args);
+	    } catch(Exception e) {
+		    System.out.println("Exception occured: " + e);
+		    System.exit(0);
+	    }
+	    System.out.println("RMI connection successful");
     }
-
 }
--- Client/src/sample/RMIInterface.java
+++
@@ -1,6 +0,0 @@
-package sample;
-
-public interface RMIInterface extends java.rmi.Remote
-{
-    public java.util.Date getDate() throws java.rmi.RemoteException;
-}
