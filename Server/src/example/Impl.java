9a751440b156b49e954a14cfe490a979c950c28c
fdcb <fdcb@live.com.pt>
2016-04-12 17:17:38 +0100

    Adding new UC feature being implemented.

--- Server/src/example/Impl.java
+++ Server/src/example/Impl.java
@@ -1,12 +1,18 @@
 package example;
-import java.rmi.*;
+
+import java.io.*;
+import java.nio.file.Files;
+import java.rmi.Naming;
+import java.rmi.RemoteException;
 import java.rmi.server.UnicastRemoteObject;
+import java.util.Vector;
 
-/*
+/**
+ * Created by Filipa on 10/04/2016.
+ */
+public class Impl extends UnicastRemoteObject implements RMIInterface{
 
-public class Impl extends UnicastRemoteObject implements RMIInterface
-{
-    public Impl(String name) throws RemoteException {
+    public Impl(String name) throws RemoteException{
         super();
         try {
             Naming.rebind(name, this);
@@ -18,9 +24,54 @@ public class Impl extends UnicastRemoteObject implements RMIInterface
                 throw new RemoteException(e.getMessage());
         }
     }
-    public java.util.Date getDate() {
-        System.out.println(" Método remoto -- Impl.getDate()");
-        return new java.util.Date();
+
+    public User validateUser(String name, String password) throws
+            RemoteException{
+
+        Vector<User> allUsers = ReadWriteFile.readFileUser();
+        for(int i = 0; i < allUsers.size(); i++)
+            if(allUsers.elementAt(i).getUsername().equals(name) && allUsers
+                    .elementAt(i).getPassword().equals(password))
+                return allUsers.elementAt(i);
+
+        return null;
+    }
+
+    public User registerUser(String name, String password, String email) throws
+            RemoteException{
+
+        Vector<User> allUsers = ReadWriteFile.readFileUser();
+        User tempUser;
+        if(allUsers != null)
+            for(int i = 0; i < allUsers.size(); i++)
+                if(allUsers.elementAt(i).getUsername().equals(name) || allUsers
+                        .elementAt(i).getEmail().equals(email))
+                    return allUsers.elementAt(i);
+
+        if(email.equals(""))
+            tempUser = new User(name, password);
+        else
+            tempUser = new User(name, password, email);
+        if(allUsers == null)
+            allUsers = new Vector <>();
+        allUsers.add(tempUser);
+        ReadWriteFile.writeFileUser(allUsers);
+        return tempUser;
+    }
+
+    public UC addUC(String className, String uniName){
+        Vector<UC> allUCs = ReadWriteFile.readFileUC();
+        for(int i = 0; i < allUCs.size(); i++)
+            if(allUCs.elementAt(i).getClass().equals(className))
+                return null;
+
+        UC newUC = null;
+        if(uniName.equals(""))
+            newUC = new UC(className);
+        else
+            newUC = new UC(className,uniName);
+        allUCs.add(newUC);
+        ReadWriteFile.writeFileUC(allUCs);
+        return newUC;
     }
 }
-*/
--- Server/src/example/ReadWriteFile.java
+++ Server/src/example/ReadWriteFile.java
@@ -9,38 +9,55 @@ import java.util.Vector;
 public class ReadWriteFile{
 
     static public Vector<User> readFileUser(){
-
         Vector<User> allUsers = new Vector <>();
-        User tempUser;
-
         File file = new File(User.USER_FILENAME);
         try{
             ObjectInputStream is = new ObjectInputStream(new FileInputStream
                     (file));
-        //    while (is.)
             allUsers = (Vector<User>)is.readObject();
-          /*  while((tempUser = (User)is.readObject())!=null){
-                allUsers.add(tempUser);
-                System.out.println("whyyy");
-            }*/
         }catch (IOException e){
             System.out.println("Error reading file");
         }
         catch (ClassNotFoundException e){
             System.exit(0);
         }
-
         return allUsers;
     }
 
     static public void writeFileUser(Vector<User> allUsers){
-
         File file = new File(User.USER_FILENAME);
         try{
             ObjectOutputStream os = new ObjectOutputStream(new FileOutputStream(
                     (file)));
-         //   for(int i = 0; i < allUsers.size(); i++)
-                os.writeObject(allUsers);
+            os.writeObject(allUsers);
+            os.flush();
+        }catch (IOException e){
+            System.out.println("Error writing in file.");
+        }
+    }
+
+    static public Vector<UC> readFileUC(){
+        Vector<UC> allUCs = new Vector <>();
+        File file = new File(UC.FILE_NAME);
+        try{
+            ObjectInputStream is = new ObjectInputStream(new FileInputStream
+                    (file));
+            allUCs = (Vector<UC>)is.readObject();
+        }catch (IOException e){
+            System.out.println("Error reading file");
+        }
+        catch (ClassNotFoundException e){
+            System.exit(0);
+        }
+        return allUCs;
+    }
+
+    static public void writeFileUC(Vector<UC> allUCs){
+        File file = new File(UC.FILE_NAME);
+        try{
+            ObjectOutputStream os = new ObjectOutputStream(new FileOutputStream(
+                    (file)));
+            os.writeObject(allUCs);
             os.flush();
         }catch (IOException e){
             System.out.println("Error writing in file.");
