61c075bdfa30c66d1fc43c796db45caadb828d60
Filipa Brito <Filipa Brito>
2016-04-14 15:13:06 +0100

    fixed buggs

--- Server/src/example/Server.java
+++ Server/src/example/Server.java
@@ -1,7 +1,29 @@
 package example;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
 import java.rmi.*;
 public class Server{
+
+    public void knockKnockServer() {
+        int portNumber = 4444;
+
+        try {
+            ServerSocket serverSocket = new ServerSocket(portNumber);
+            Socket clientSocket = serverSocket.accept();
+            PrintWriter out =
+                    new PrintWriter(clientSocket.getOutputStream(), true);
+            BufferedReader in = new BufferedReader(
+                    new InputStreamReader(clientSocket.getInputStream()));
+            clientSocket = serverSocket.accept();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
     public static void main(String[] argv) {
         System.setProperty("java.security.policy", "C:\\T01SD\\Server\\src" +
